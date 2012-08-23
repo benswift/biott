3719dcac742f8d41676b0988fad8a97afcc3c06d
Ben Swift <benjamin.j.swift@gmail.com>
2012-08-24 09:26:55 +1000

    added foolib dynamic library files

---
+++ extras/foolib/foolib.c
@@ -0,0 +1,7 @@
+/* foolib.c */
+
+#include "foolib.h"
+
+int64_t foo(int64_t bar){
+  return bar + 42;
+}
---
+++ extras/foolib/foolib.h
@@ -0,0 +1,5 @@
+/* foolib.h */
+
+#include <sys/types.h> /* to provide the i64_t type */
+
+int64_t foo(int64_t bar);
